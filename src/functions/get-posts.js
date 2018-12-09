45a3da09a0c68bb751dca8fd7802a21acc7040df
jhackett1 <joshua.hackett93@gmail.com>
2018-12-09 02:53:22 +0000

    projects lambda works at least

--- src/functions/get-posts.js
+++ src/functions/get-posts.js
@@ -1,14 +1,31 @@
-import fetch from 'isomorphic-unfetch'
+import fetch from "node-fetch"
+import convert from 'xml-to-json-promise'
 
 const mediumFeed = `https://medium.com/feed/@dinosaurlord`
 
 exports.handler = async (event, context) => {
-    const mediumRes = await fetch(mediumFeed)
-    const mediumXML = await mediumRes.text()
-    let mediumPosts = await xml2js(mediumXML)
-    mediumPosts = mediumPosts.rss.channel[0].item
-    return {
-        statusCode: 200,
-        body: mediumPosts
-    }
-}
+
+        const response = await fetch(mediumFeed)
+        const text = await response.text()
+        convert.xmlDataToJSON(text)
+            .then(fuck => {
+                return {
+                    statusCode: 200,
+                    body: fuck
+                }
+            })
+            .catch(err => { 
+                return{
+                    statusCode: 400,
+                    body: err
+                }
+            })
+
+
+};
+
+
+
+// const mediumXML = await mediumRes.text()
+// let mediumPosts = await xml2js(mediumXML)
+// mediumPosts = mediumPosts.rss.channel[0].item
--- src/functions/get-projects.js
+++ src/functions/get-projects.js
@@ -1,12 +1,13 @@
-import fetch from 'isomorphic-unfetch'
+import fetch from "node-fetch";
 
 const behanceEndpoint = `https://www.behance.net/v2/users/jhackett1/projects?api_key=${process.env.BEHANCE_API_KEY}`
 
 exports.handler = async (event, context) => {
-  const behanceRes = await fetch(behanceEndpoint)
-  const behancePosts = await behanceRes.json()
-  return {
-    statusCode: 200,
-    body: behancePosts
-  }
-}
+  return fetch(behanceEndpoint)
+    .then(response => response.json())
+    .then(data => ({
+      statusCode: 200,
+      body: data
+    }))
+    .catch(error => ({ statusCode: 422, body: String(error) }));
+};
