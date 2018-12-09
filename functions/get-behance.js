273e9d1390e10db05c680053bfbac91a7ea1258f
jhackett1 <joshua.hackett93@gmail.com>
2018-12-09 01:17:39 +0000

    require in functions

--- functions/get-behance.js
+++ functions/get-behance.js
@@ -1,4 +1,4 @@
-import fetch from "node-fetch"
+const fetch = require("node-fetch")
 
 const behanceEndpoint = `https://www.behance.net/v2/users/jhackett1/projects?api_key=${process.env.BEHANCE_API_KEY}`
 
